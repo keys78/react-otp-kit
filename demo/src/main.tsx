4b41b6e92cf1a93de42e9660f6ddeecf183abf67
keys78 <rahzy24@gmail.com>
2024-05-15 10:44:40 +0100

    fix

--- demo/src/main.tsx
+++ demo/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import '../src/assets/styles/tailwind.css'
-import '../src/assets/styles/index.css'
+import './index.css'
+import './tailwind.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
