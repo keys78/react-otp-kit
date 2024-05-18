a248b93f13bc3cf6cdbb1197d5b5bdac4ab3aaac
Hannahadora <hannahadora97@gmail.com>
2024-05-18 23:48:24 +0100

    set active path

--- demo/src/components/BreadCrumbs.tsx
+++ demo/src/components/BreadCrumbs.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const BreadCrumbs = () => {
   const location = useLocation();
   const navigate = useNavigate()
 
+  const [activePath, setActivePath] = useState('') 
+
   const paths = location.pathname.split('/');
 
   const navigateTo = (index: number) => {
-    const path = '/' + paths[index];
-    navigate(path);
+    navigate('/' + paths[index]);
+    setActivePath('/' + paths[index])
   };
+
+  useEffect(() => {
+    setActivePath(location.pathname)
+  }, [location.pathname])
 
   return (
     <div className='capitalize mb-[16px]'>
       {paths?.map((path, index) => (
         <React.Fragment key={index}>
           <span 
-            className={`${index !== paths?.length - 1 && 'cursor-pointer'}`}
+            className={`${index !== paths?.length - 1 && 'cursor-pointer'} ${'/' + path === activePath && 'text-blue-500'}`}
             onClick={() => navigateTo(index)}
           >
             {path === '' ? 'Home' : path }
