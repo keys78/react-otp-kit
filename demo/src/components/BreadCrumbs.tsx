9cccfe4ef77d0db22d90754f9d3ec2f35d3cdf87
Hannahadora <hannahadora97@gmail.com>
2024-05-20 15:36:17 +0100

    fix icon

--- demo/src/components/BreadCrumbs.tsx
+++ demo/src/components/BreadCrumbs.tsx
@@ -48,18 +48,9 @@ const BreadCrumbs = () => {
             {path === "" ? "Home" : path}
           </span>
           <span
-            className={`px-[4px] ${index === paths?.length - 1 && "hidden"}`}
+            className={`px-[4px] mb-[-5px] ${index === paths?.length - 1 && "hidden"}`}
           >
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              fill="rgba(246,245,245,1)"
-            >
-              <path d="M12.1717 12.0005L9.34326 9.17203L10.7575 7.75781L15.0001 12.0005L10.7575 16.2431L9.34326 14.8289L12.1717 12.0005Z"></path>
-            </svg> */}
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" viewBox="0 0 256 256"><path d="M181.66,133.66l-80,80a8,8,0,0,1-11.32-11.32L164.69,128,90.34,53.66a8,8,0,0,1,11.32-11.32l80,80A8,8,0,0,1,181.66,133.66Z"></path></svg>
           </span>
         </React.Fragment>
       ))}
